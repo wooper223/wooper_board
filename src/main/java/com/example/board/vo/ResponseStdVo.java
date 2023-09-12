510d0f6d236fab0682ec879e60be0f54a713baf4
YuHayeon <youha0930@gmail.com>
2023-09-12 22:50:03 +0900

    refactor: response -> data 변수명 변경

--- src/main/java/com/example/board/vo/ResponseStdVo.java
+++ src/main/java/com/example/board/vo/ResponseStdVo.java
@@ -11,19 +11,19 @@ public class ResponseStdVo<T> {
 
 	private final String message;
 
-	private final T response;
+	private final T data;
 
 	public ResponseStdVo(HttpStatus status, String message) {
 		this(status, message, null);
 	}
 
-	public ResponseStdVo(HttpStatus status, T response) {
-		this(status, "", response);
+	public ResponseStdVo(HttpStatus status, T data) {
+		this(status, "", data);
 	}
 
-	public ResponseStdVo(HttpStatus status, String message, T response) {
+	public ResponseStdVo(HttpStatus status, String message, T data) {
 		this.status = status;
 		this.message = message;
-		this.response = response;
+		this.data = data;
 	}
 }
