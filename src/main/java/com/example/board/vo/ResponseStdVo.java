040eab8ad3b477f284cba82ccbe8e34ef704b428
YuHayeon <youha0930@gmail.com>
2023-09-12 22:48:14 +0900

    add: 공통 Response 처리할 수 있는 ResponseStdVo 추가

---
+++ src/main/java/com/example/board/vo/ResponseStdVo.java
@@ -0,0 +1,29 @@
+package com.example.board.vo;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class ResponseStdVo<T> {
+
+	private final HttpStatus status;
+
+	private final String message;
+
+	private final T response;
+
+	public ResponseStdVo(HttpStatus status, String message) {
+		this(status, message, null);
+	}
+
+	public ResponseStdVo(HttpStatus status, T response) {
+		this(status, "", response);
+	}
+
+	public ResponseStdVo(HttpStatus status, String message, T response) {
+		this.status = status;
+		this.message = message;
+		this.response = response;
+	}
+}
